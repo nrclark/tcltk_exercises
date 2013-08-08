fcd21b03ecff4a16d591efc0aa90c9ae5f6fa0a2
Nicholas Clark <nicholas.clark@gmail.com>
2013-08-07 23:24:04 -0700

    Did 203. Fast one.

---
+++ ch3/203.tcl
@@ -0,0 +1,18 @@
+#!/usr/bin/env tclsh
+
+proc checkPalindrome {input} {
+    puts $input
+    
+    set reverse [string reverse $input]
+    if {[string compare -nocase $input $reverse]} {
+        return 0
+    }
+    return 1
+}
+
+proc main {} {
+    gets stdin myInput
+    puts [checkPalindrome $myInput]
+}
+
+main
